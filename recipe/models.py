1a64065f9465c766897b90d51c4b86ec75ade43b
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-02-26 22:32:52 +0100

    Add Recipe model

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=128)
+    # category TODO add a category for a recipe
+    note = models.IntegerField(max_length=2)
+    hard = models.IntegerField(max_length=2)
+    serves = models.IntegerField(max_length=2)
+    preparationTime = models.IntegerField(max_length=4)
+    cuissonTime = models.IntegerField(max_length=4)
+    preparation = models.TextField()
+    date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date d'ajout")
+
+    def __unicode__(self):
+        """
+        Cette méthode que nous définirons dans tous les modèles
+        nous permettra de reconnaître facilement les différents objets que nous
+        traiterons plus tard et dans l'administration
+        """
+    return u"%s" % self.title
