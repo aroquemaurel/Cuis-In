92f4e0dbf7fbdfade13bd4a81f7c980c77ed87d5
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-02-26 22:27:48 +0100

    Add files for recipe list

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,8 @@
+#-*- coding: utf-8 -*-
+
+from datetime import datetime
 from django.shortcuts import render
 
-# Create your views here.
+
+def list(request):
+    return render(request, 'recipe/list.html', {'current_date': datetime.now()})
