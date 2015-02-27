d0ead766a56e29dab4b6d7348266232a61e2b9b5
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-02-27 09:10:35 +0100

    Add skeleton template

--- cuisin/settings.py
+++ cuisin/settings.py
@@ -81,8 +81,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
-
+STATIC_URL = '/assets/'
+STATICFILES_DIRS = (
+    "/home/aroquemaurel/projets/python/Cuis-In/assets/",
+)
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
