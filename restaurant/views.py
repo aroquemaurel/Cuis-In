730785ce7d27ae907259f2f9f7deef28914f8e82
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-03-12 14:51:39 +0100

    Beginnig of template

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from restaurant.models import Restaurant
 
 def list(request):
-    return render(request, 'restaurant/list.html')
+    restaurants = Restaurant.objects.all().order_by('title')
+
+    return render(request, 'restaurant/list.html', {'restaurants': restaurants})
 
