4b09d746e51a365da0564289eeb2696b8ea20700
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-10-25 01:06:16 +0200

    Template of pagination #9

--- recipe/templatetags/recipe_extras.py
+++ recipe/templatetags/recipe_extras.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from django import template
+from django.template import TemplateSyntaxError, Variable
 from django.utils.safestring import mark_safe
+from django.template import Library, Node, TemplateSyntaxError
 
 register = template.Library()
 
@@ -54,3 +56,43 @@ def hash(h, key):
         return h[key]
     else:
         return None
+
+
+class RangeNode(Node):
+    def __init__(self, num, context_name):
+        self.num = Variable(num)
+        self.context_name = context_name
+
+    def render(self, context):
+        context[self.context_name] = range(int(self.num.resolve(context)))
+        return ""
+
+@register.tag
+def num_range(parser, token):
+    """
+    Takes a number and iterates and returns a range (list) that can be
+    iterated through in templates
+
+    Syntax:
+    {% num_range 5 as some_range %}
+
+    {% for i in some_range %}
+      {{ i }}: Something I want to repeat\n
+    {% endfor %}
+
+    Produces:
+    0: Something I want to repeat
+    1: Something I want to repeat
+    2: Something I want to repeat
+    3: Something I want to repeat
+    4: Something I want to repeat
+    """
+    try:
+        fnctn, num, trash, context_name = token.split_contents()
+    except ValueError:
+        raise TemplateSyntaxError("%s takes the syntax %s number_to_iterate\
+            as context_variable" % (fnctn, fnctn))
+    if not trash == 'as':
+        raise TemplateSyntaxError("%s takes the syntax %s number_to_iterate\
+            as context_variable" % (fnctn, fnctn))
+    return RangeNode(num, context_name)
