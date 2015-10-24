3eb3e8be4478ab3f433897b6a28d5801425cfec1
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-10-25 00:18:14 +0200

    Search is now using tags #5

--- search/views.py
+++ search/views.py
@@ -1,14 +1,18 @@
+from django.db.models import Q
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
 from recipe.models import Recipe
+from tags.models import Tag
 
 
 def search(request):
     recipes = Recipe.objects.all()
+    tags = Tag.objects.all()
     search_text = request.GET.get('s', '')
     if search_text != '':
-        recipes = recipes.filter(title__icontains=search_text)
+        id_tags = tags.values_list('id').filter(tag__icontains=search_text)
+        recipes = recipes.filter(Q(title__icontains=search_text) |Q(tags__id__in=id_tags))
     else:
         return HttpResponseNotFound('<h1>Page not found</h1>')
     
