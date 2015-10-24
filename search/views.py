869d97f6e929943b54fb6e6b45cc9597d9f9b499
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-10-24 23:41:06 +0200

    Add empty menu in leftbar #5

--- search/views.py
+++ search/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.db.models import Q
 
 from recipe.models import Recipe
 
@@ -7,4 +6,5 @@ from recipe.models import Recipe
 def search(request, searchtext):
     recipes = Recipe.objects.all()
     recipes = recipes.filter(title__icontains=searchtext)
+    
     return render(request, 'search/search.html', {'recipes': recipes})
